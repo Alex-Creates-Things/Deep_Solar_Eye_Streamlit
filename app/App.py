79bb9a1d056d4d14d8d3a662cdc2262ca3ae014c
Alexander Simon <alexander.t.simon@gmail.com>
2024-02-15 14:36:45 +0100

    Added loader

--- app/App.py
+++ app/App.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import time
 from io import BytesIO
 from PIL import Image
 
@@ -28,15 +29,16 @@ if uploaded_file is not None:
     file = {"file": (uploaded_file.name, byte_arr, "image/jpeg")}
 
     response = requests.post("https://deep-solar-eye-xzsdeienxq-ew.a.run.app/predict", files=file)
-    
+    with st.spinner('Calculating power loss...'):
+        time.sleep(2)
     if response.status_code == 200:
         st.write("	:crystal_ball:")
         prediction_result = response.json()
         percent_loss = round(float(prediction_result['power_loss']) * 100, 2)
         if percent_loss < 0:
             percent_loss = 0
-            st.write("Power Loss Prediction:")
-            st.write(f"Power Loss: {percent_loss}%")
+        st.write("Power Loss Prediction:")
+        st.write(f"Power Loss: {percent_loss}%")
     else:
         st.write("  :construction:")
         st.write("Failed to get prediction result")
